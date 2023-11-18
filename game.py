9a12d896b16210f37cf2cb127f45c1ba7ab418ac
Olateju <3legged01@gmail.com>
2023-11-18 07:40:01 +0000

    game.py modified by changing the display of characters

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 first_name = input("Enter your first name: ").capitalize()
 last_name = input("Enter your last name: ").capitalize()
+length_fname = len(first_name)
+length_lname = len(last_name)
 print(f"Your first name is   ", first_name) 
 print(f"Your last name is   ", last_name) 
 print(f"Your full name is  {first_name} {last_name}")
 print(f"Your full name can also be  {last_name} {first_name}")
-print(f"The length of your first name is ", len(first_name))
-print(f"The length of your last name is ", len(last_name))
+print(f"Your  first name {first_name} has {length_fname} characters")
+print(f"Your  last name {last_name} has {length_lname} characters")
 
 
